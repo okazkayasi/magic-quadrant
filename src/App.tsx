cff6433c6b9719f6f452f1a003cb75d258138549
okazkayasi <oguz.kazkayasi@saypr.com>
2022-04-11 11:51:38 +0300

    feat: table completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,55 @@
 import Chart from "./components/Chart";
 import Table from "./components/Table";
 import "./App.css";
+import { useState } from "react";
+
+export type Data = {
+  id: string;
+  label: string;
+  vision: number;
+  ability: number;
+};
 
 function App() {
+  const [dataList, setDataList] = useState([] as Data[]);
+
+  const addNewLine = () => {
+    let new_id = 0;
+    if (dataList.length > 0) {
+      const all_ids = dataList.map((d) => parseInt(d.id));
+      const last_id = Math.max(...all_ids);
+      new_id = last_id + 1;
+    }
+
+    setDataList((dataList) => [
+      ...dataList,
+      { id: new_id + "", label: "", vision: 0, ability: 0 },
+    ]);
+  };
+
+  const changeData = (data: Data) => {
+    const data_ind = dataList.findIndex((d) => d.id === data.id);
+    setDataList((dataList) => [
+      ...dataList.slice(0, data_ind),
+      data,
+      ...dataList.slice(data_ind + 1),
+    ]);
+  };
+
+  const deleteLine = (id: string) => {
+    setDataList((dataList) => dataList.filter((d) => d.id !== id));
+  };
+
+  console.log(dataList, "datalist");
   return (
     <div className="App">
       <Chart />
-      <Table />
+      <Table
+        data_list={dataList}
+        addNewLine={addNewLine}
+        deleteLine={deleteLine}
+        changeData={changeData}
+      />
     </div>
   );
 }
--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -1,14 +1,98 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Table.css";
+import { Data } from "../../App";
 
-const TableRow = () => {
-  return <div></div>;
+const TableRow: React.FC<{
+  data: Data;
+  deleteLine: (id: string) => void;
+  changeData: (data: Data) => void;
+}> = ({ data, deleteLine, changeData }) => {
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+
+    if (name !== "label") {
+      // check if value is in limits
+      const value_int = parseInt(value);
+      if (value_int < 0 || value_int > 100) {
+        return;
+      }
+    }
+    changeData({ ...data, [name]: name === "label" ? value : parseInt(value) });
+  };
+
+  return (
+    <div className="table_row">
+      <div className="input_wrapper">
+        <input
+          type="text"
+          name="label"
+          value={data.label}
+          onChange={onChange}
+        />
+      </div>
+      <div>
+        <input
+          type="number"
+          step={0.01}
+          min="0"
+          max="100"
+          name="vision"
+          value={data.vision}
+          onChange={onChange}
+        />
+      </div>
+      <div>
+        <input
+          type="number"
+          step={0.01}
+          min="0"
+          max="100"
+          name="ability"
+          value={data.ability}
+          onChange={onChange}
+        />
+      </div>
+      <div>
+        <button onClick={() => deleteLine(data.id)}>Delete</button>
+      </div>
+    </div>
+  );
 };
 
-const Table = () => {
+const Table: React.FC<{
+  data_list: Data[];
+  addNewLine: () => void;
+  deleteLine: (id: string) => void;
+  changeData: (data: Data) => void;
+}> = ({ data_list, addNewLine, deleteLine, changeData }) => {
+  console.log(data_list, "here");
   return (
-    <div>
+    <div className="table_container">
+      <div className="add_button_wrapper">
+        <button onClick={addNewLine}>Add</button>
+      </div>
       <div className="table_head">
-
+        <div className="label_container">
+          <p>Label</p>
+        </div>
+        <div>
+          <p>Vision</p>
+        </div>
+        <div>
+          <p>Ability</p>
+        </div>
+        <div>
+          <p>Delete</p>
+        </div>
+      </div>
+      <div className="table_body">
+        {data_list.map((data) => (
+          <TableRow
+            data={data}
+            deleteLine={deleteLine}
+            changeData={changeData}
+          />
+        ))}
       </div>
     </div>
   );
